0b00d0fd1a8297808cec34f16a50929d415ba30c
hyungyun.Moon <mhg5303@gmail.com>
2018-04-14 23:33:23 +0900

    Implement JpaItemRepository

--- src/main/java/com/hg/chat/store/persistence/JpaItemRepository.java
+++ src/main/java/com/hg/chat/store/persistence/JpaItemRepository.java
@@ -1,5 +1,22 @@
 package com.hg.chat.store.persistence;
 
-public class JpaItemRepository {
+import javax.persistence.EntityManager;
+import javax.persistence.EntityManagerFactory;
 
+import com.hg.chat.store.domain.Item;
+import com.hg.chat.store.domain.ItemRepository;
+
+public class JpaItemRepository {
+	private EntityManagerFactory entityManagerFactory;
+
+	public void setEntityManagerFactory(EntityManagerFactory emf) {
+		this.entityManagerFactory = emf;
+	}
+
+	@Override
+	public Item findById(Integer itemId) {
+		EntityManager entityManager = entityManagerFactory.createEntityManager();
+		entityManager.joinTransaction();
+		return entityManager.find(Item.class, itemId);
+	}
 }
