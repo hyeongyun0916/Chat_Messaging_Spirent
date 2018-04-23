34f53c498b71fe82f0408135142d9761afc4636c
hyungyun.Moon <mhg5303@gmail.com>
2018-04-24 01:30:00 +0900

    add getallchatbyid doxygen

--- Server/Server/Doxygen/html/search/all_4.js
+++ Server/Server/Doxygen/html/search/all_4.js
@@ -1,5 +1,6 @@
 var searchData=
 [
   ['getallchat',['getAllChat',['../class_d_b_manager.html#a546cf5b82fe9cb115cfee8d52f90f0da',1,'DBManager']]],
+  ['getallchatbyid',['getAllChatbyID',['../class_d_b_manager.html#af90bbc5f97ee7bb051006178c5e6834b',1,'DBManager']]],
   ['getalluser',['getAllUser',['../class_d_b_manager.html#a0171d3489ef375c9eeff4014a4a485de',1,'DBManager']]]
 ];
